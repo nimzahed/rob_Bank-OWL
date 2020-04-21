bcc3abd53514ba5d1c2161a5cd1e2f682aba5ac5
nimzahed <63586241+nimzahed@users.noreply.github.com>
2020-04-22 00:20:02 +0430

    Add files via upload

---
+++ setting.lua
@@ -0,0 +1,28 @@
+﻿--position marker hack
+interiorhack = 0 --interior
+dimensionhack = 0 -- mikhay tooye che بعدی bashe?
+hackx , hacky , hackz = 1551.1009521484, -1342.7038574219, -35.769644165039 -- positioni ke mikhay
+--position marker haye rob
+marker1x , marker1y , marker1z = 1546.6302490234, -1339.2565917969, -35.769644165039
+marker2x , marker2y , marker2z = 1546.6116943359, -1335.225341796 , -35.769644165039
+marker3x , marker3y , marker3z = 1546.8118896484, -1331.042724609 , -35.769644165039
+marker4x , marker4y , marker4z = 1551.9359130859, -1331.287841796,  -35.769644165039
+marker5x , marker5y , marker5z = 1551.8460693359, -1335.726318359 , -35.769644165039
+marker6x , marker6y , marker6z = 1551.5482177734, -1339.162841795 , -35.769644165039
+interiormarker  = 0 -- interior rob ha
+dimensionmarker = 0 -- بعد rob ha
+--dar gav sandogh
+interiordar  = 0 
+dimensiondar = 0
+darx , dary , darz = 1549.1175537109, -1341.6970214844, -34.390145874023
+--vaghti hack tamom shod dar gav sandogh kodom var bere?
+targetdarx , targetdary , targetdarz = 1551.1175537109, -1341.6970214844, -34.390145874023
+--pool rob(pooli ke har miz mide na pool kol rob)
+hadaghapoolrob = 10000
+bishtarinpoolrob = 15000
+--pooli ke mikhay hardafe az miz vardare
+poolvardashtan = 500
+--had aghal tedad pd 
+hadaghaltedadpd  = 3
+--reset kardan rob
+timeresetrob = 1 --saat
